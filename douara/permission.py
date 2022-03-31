05426128cfc0ee5181607d18c27ded71da902a4c
mghoneem10 <mghoneem10@gmail.com>
2022-03-31 05:46:36 +0200

    server migration

--- douara/permission.py
+++ douara/permission.py
@@ -8,6 +8,7 @@ from frappe.share import add
 
 @frappe.whitelist()
 def share_lead(doc, method=None):
+    '''
     users = frappe.db.sql(""" select owner from `tabToDo` where reference_type = 'Lead' and reference_name = '{name}' """.format(name=doc.reference_name), as_dict=1)
     read = 1
     write = 1
@@ -15,3 +16,4 @@ def share_lead(doc, method=None):
     everyone = 0
     for x in users:
         add('Lead', doc.reference_name, x.user, read, write, share, everyone)
+'''
